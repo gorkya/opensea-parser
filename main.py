8a0cbe19e975db1254d63fd468d966f224398229
= <=>
2022-08-04 12:02:40 +0500

    add english comments

--- main.py
+++ main.py
@@ -7,20 +7,18 @@ import pandas as pd
 
 url = 'https://opensea.io/explore-collections'
 
-# driver – браузер, которому мы будем отдавать команды
 s = Service("C:/Users/anast/PycharmProjects/opensea-parser/chromedriver/chromedriver.exe")
 driver = webdriver.Chrome(service=s)
 
-# driver будет ждать максимум 10 сек перед тем, как появится какой-то элемент
 driver.implicitly_wait(10)
 
-# для сбора нужной информации и передачи в csv
+# to collect the necessary information and transfer to csv
 info_dict = {'name': [],
              'best offer': [],
              'href': []}
 
 
-# функция проверяет, есть ли вообще элемент со статистикой на странице
+# the function checks if there is an element with statistics on the page
 def check_exists_by_xpath(xpath):
     try:
         driver.find_element(By.XPATH, xpath)
@@ -29,27 +27,26 @@ def check_exists_by_xpath(xpath):
     return True
 
 
-# здесь наши запросы
 try:
     driver.get(url=url)
 
     h_prev = 0
-    # цикл прокрутки
+    # scroll cycle
     while True:
-        # прокручиваем вниз
+        # scroll down
         h = driver.execute_script('return window.innerHeight + window.scrollY;')
         driver.execute_script(f'window.scrollTo(0,{h});')
         time.sleep(2)
 
-        # получаем блок из всех NFT
+        # get a block of all NFT
         block = driver.find_element(By.CSS_SELECTOR, '#main > div > div:nth-child(3) > div > div > div')
 
-        # получаем названия всех NFT и добавляем в общий список, проверяя на отсутствие в списке
+        # get all NFT names, check for absence in the list and add to the list
         for name in block.text.split('\n'):
             if name not in info_dict['name']:
                 info_dict['name'].append(name)
 
-        # получаем ссылки на NFT и добавляем в общий список, проверяя на отсутствие в списке
+        # get NFT urls, check for absence in the list and add to the list
         elements = block.find_elements(
             By.CSS_SELECTOR, '#main > div > div:nth-child(3) > div > div > div > div > div > a'
         )
@@ -58,12 +55,12 @@ try:
             if href not in info_dict['href']:
                 info_dict['href'].append(href)
 
-        # новую высоту прокрутки сравниваем со старой
+        # compare the new scroll height with the old one
         if h == h_prev:
             break
         h_prev = h
 
-    # открываем страницы NFT и вытаскиваем статистику, проверяя её наличие на странице
+    # open NFT pages, check statistics existence, add to the list
     for href in info_dict['href']:
         driver.get(href)
         if check_exists_by_xpath('//*[@id="main"]/div/div/div[5]/div/div[1]/div/div[3]/div/div[10]/a/div/span[1]/div'):
@@ -74,15 +71,13 @@ try:
         else:
             info_dict['best offer'].append(None)
 
-# здесь будем выводить ошибки, если будут
 except Exception as ex:
     print(ex)
 
-# здесь код закрытия драйвера, который должен выполняться всегда, иначе будет куча болтающихся процессов в системе
 finally:
-    driver.close()      # закрывает вкладку
-    driver.quit()       # закрывает браузер
+    driver.close()
+    driver.quit()
 
-# записываем данные в csv файл
+# write data to csv file
 df = pd.DataFrame(info_dict)
 df.to_csv('NFT.csv')
